5f0b57ddf96023f01527917086978bb108ad0dc4
Ronald Koster <ronald@fam-koster.nl>
2021-11-28 20:49:20 +0100

    Add plus and minus and multiple decades to addition game.

--- src/AdditionSubstractionWithinDecadeApp.ts
+++ src/AdditionSubstractionWithinDecadeApp.ts
@@ -3,9 +3,15 @@ import { customElement, state } from 'lit/decorators.js';
 import type { CSSResultGroup, HTMLTemplateResult } from 'lit';
 
 import { TimeLimitedGame } from './TimeLimitedGame';
-import { randomFromSetAndSplice, randomIntFromRange } from './Randomizer';
+import {
+  randomFromSet,
+  randomFromSetAndSplice,
+  randomIntFromRange,
+} from './Randomizer';
 import './AscendingBalloons';
 import type { Answers, AscendingBalloons } from './AscendingBalloons';
+
+type Operator = '+' | '-';
 
 @customElement('addition-substraction-within-decade-app')
 export class AdditionSubstractionWithinDecadeApp extends TimeLimitedGame {
@@ -14,11 +20,50 @@ export class AdditionSubstractionWithinDecadeApp extends TimeLimitedGame {
   @state()
   private secondNumber = 1;
   @state()
-  private operator: '+' | '-' = '+';
+  private operator: Operator = '+';
   @state()
   private answers: Answers = { correct: 1, incorrect: [2, 3, 4] };
   @state()
   private gameElementsDisabled = true;
+
+  private decades: number[] = [];
+  private operators: Operator[] = [];
+
+  constructor() {
+    super();
+    this.parseUrl();
+  }
+
+  private parseUrl(): void {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const decadesFromUrl = urlParams.getAll('decade');
+    decadesFromUrl.forEach(decadeAsString => {
+      const decade = parseInt(decadeAsString, 10);
+      if (
+        !Number.isNaN(decade) &&
+        decade >= 0 &&
+        decade % 10 === 0 &&
+        decade < 100 &&
+        !this.decades.find(value => value === decade)
+      ) {
+        this.decades.push(decade);
+      }
+    });
+    if (this.decades.length === 0) this.decades.push(0);
+
+    const operatorsFromUrl = urlParams.getAll('operator');
+    operatorsFromUrl.forEach(operator => {
+      if (operator === 'plus' && !this.operators.find(value => value === '+'))
+        this.operators.push('+');
+      else if (
+        operator === 'minus' &&
+        !this.operators.find(value => value === '-')
+      )
+        this.operators.push('-');
+    });
+    if (this.operators.length === 0) this.operators.push('+');
+  }
 
   /** Get the ascending balloons child */
   private get ascendingBalloons(): AscendingBalloons {
@@ -49,12 +94,45 @@ export class AdditionSubstractionWithinDecadeApp extends TimeLimitedGame {
 
   /** Get the text to show in the game over dialog */
   get welcomeMessage(): HTMLTemplateResult {
-    return html`<p>Klik op de ballon met het juiste antwoord</p> `;
+    const exerciseExamples: string[] = [];
+    let exerciseExamplesAsScentence = '';
+
+    if (this.operators.find(value => value === '+')) {
+      this.decades.forEach(decade => exerciseExamples.push(`${decade + 3}+4`));
+    }
+    if (this.operators.find(value => value === '-')) {
+      this.decades.forEach(decade => exerciseExamples.push(`${decade + 7}-5`));
+    }
+
+    if (exerciseExamples.length <= 0) throw new Error('Internal error');
+    else if (exerciseExamples.length === 1)
+      exerciseExamplesAsScentence = exerciseExamplesAsScentence.concat(
+        `${exerciseExamples[0]}.`
+      );
+    else {
+      exerciseExamples.forEach((value, index) => {
+        if (index === 0) {
+          exerciseExamplesAsScentence =
+            exerciseExamplesAsScentence.concat(value);
+        } else if (index === exerciseExamples.length - 1) {
+          exerciseExamplesAsScentence = exerciseExamplesAsScentence.concat(
+            ` en ${value}.`
+          );
+        } else {
+          exerciseExamplesAsScentence = exerciseExamplesAsScentence.concat(
+            `, ${value}`
+          );
+        }
+      });
+    }
+
+    return html`<p>Sommen als ${exerciseExamplesAsScentence}</p>
+      <p>Klik op de ballon met het juiste antwoord.</p> `;
   }
 
   /** Get the title for the welcome dialog. */
   get welcomeDialogTitle(): string {
-    return `Sommen zoals 3+4`;
+    return `Sommen binnen het tiental`;
   }
 
   private handleCorrectAnswer(): void {
@@ -72,12 +150,27 @@ export class AdditionSubstractionWithinDecadeApp extends TimeLimitedGame {
   }
 
   private newRound() {
-    this.firstNumber = randomIntFromRange(1, 9);
-    this.secondNumber = randomIntFromRange(1, 10 - this.firstNumber);
-    this.operator = '+';
-    const answer = this.firstNumber + this.secondNumber;
+    this.operator = randomFromSet(this.operators);
+    const decade = randomFromSet(this.decades);
+    let answer: number;
+
+    if (this.operator === '+') {
+      this.firstNumber = randomIntFromRange(0 + decade, 9 + decade);
+      this.secondNumber = randomIntFromRange(
+        0,
+        10 - (this.firstNumber - decade)
+      );
+      answer = this.firstNumber + this.secondNumber;
+    } else if (this.operator === '-') {
+      this.firstNumber = randomIntFromRange(1 + decade, 10 + decade);
+      this.secondNumber = randomIntFromRange(0, this.firstNumber - decade);
+      answer = this.firstNumber - this.secondNumber;
+    } else {
+      throw new Error('Unsupported operator found');
+    }
+
     const possibleAnswers = [];
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 0 + decade; i <= 10 + decade; i++) {
       if (i !== answer) possibleAnswers.push(i);
     }
     this.answers = {
