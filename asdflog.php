2e0e0d12f0484c839e1ada4e762cc69c66bb4e09
Ronald Koster <ronald@fam-koster.nl>
2022-05-29 16:56:05 +0200

    Log referring domain in gamecounts.

--- asdflog.php
+++ asdflog.php
@@ -1,7 +1,27 @@
 <?php
 
 $game = $_GET["game"];
-$log  = date("c").",".$game.PHP_EOL;
+$ref=@$_SERVER[HTTP_REFERER];
+
+echo "Test";
+
+if ($ref == "http://localhost:8000/"){
+    $refCode = "L";
+}
+elseif ($ref == "https://jufankie.nl/"){
+    $refCode = "J";
+}
+elseif ($ref == "https://jufankie.github.io/") {
+    $refCode = "G";
+}
+else{
+    $refCode = "O";   
+}
+
+$log  = date("c").",".$refCode.",".$game.PHP_EOL;
+
+echo $log;
+
 //Save string to log, use FILE_APPEND to append.
 file_put_contents('./log_'.date("Ym").'.log', $log, FILE_APPEND);
 
