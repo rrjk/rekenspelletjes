a6cf47a3d6d7c9b0ee7f13ee8aca84d8c0fb9ab1
Ronald Koster <ronald@fam-koster.nl>
2023-09-16 13:02:00 +0200

    Add color2k library and add small test app.

--- src/TestApp.ts
+++ src/TestApp.ts
@@ -3,35 +3,43 @@ import { html, css, LitElement, unsafeCSS } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import type { CSSResultArray, HTMLTemplateResult } from 'lit';
 
-import chroma from 'chroma-js';
+import { darken, lighten } from 'color2k';
 
-import './RealHeight';
+// import './RealHeight';
 
 @customElement('test-app')
 export class TestApp extends LitElement {
-  /** Get all static styles */
   static get styles(): CSSResultArray {
     const a = css`
       #div1 {
-        height: 400px;
+        height: 150px;
         width: 400px;
         color: black;
         border: 2px black solid;
-        background-color: red;
+        background-color: #e6194b;
       }
       #div2 {
-        height: 400px;
+        height: 150px;
         width: 400px;
         color: black;
         border: 2px black solid;
-        background-color: ${unsafeCSS(chroma('red').darken().hex())};
+        background-color: ${unsafeCSS(darken('#E6194B', 0.1))};
+      }
+      #div3 {
+        height: 150px;
+        width: 400px;
+        color: black;
+        border: 2px black solid;
+        background-color: ${unsafeCSS(lighten('#E6194B', 0.1))};
       }
     `;
+
     return [a];
   }
 
   protected render(): HTMLTemplateResult {
     return html` <div id="div1">test1</div>
-      <div id="div2">test2</div>`;
+      <div id="div2">test2</div>
+      <div id="div3">Test3</div>`;
   }
 }
--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,8 +1,24 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
 
+// The following two are includes to be able to import old-style CommonJs modules
+// See https://modern-web.dev/guides/dev-server/using-plugins/#commonjs
+// The modules for which this should work have to explicitely mentioned in the commonjs plugin (see below)
+import { fromRollup } from '@web/dev-server-rollup';
+import rollupCommonjs from '@rollup/plugin-commonjs';
+
+const commonjs = fromRollup(rollupCommonjs);
+
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   nodeResolve: true,
   open: '/',
 
-  plugins: [esbuildPlugin({ ts: true, target: 'auto' })],
+  plugins: [
+    commonjs({
+      include: [
+        '**/node_modules/colors/**/*',
+        '**/node_modules/chroma-js/**/*',
+      ],
+    }),
+    esbuildPlugin({ ts: true, target: 'auto' }),
+  ],
 });
