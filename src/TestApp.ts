77d99299d5cb1ee5acd3b6dda53a1283838728d3
Ronald Koster <ronald@fam-koster.nl>
2023-11-05 11:15:03 +0100

    Create custome element for hand with dots.

--- src/TestApp.ts
+++ src/TestApp.ts
@@ -4,42 +4,166 @@ import { customElement } from 'lit/decorators.js';
 import type { CSSResultArray, HTMLTemplateResult } from 'lit';
 
 import { darken, lighten } from 'color2k';
-import { getZeppelinAsSvgUrl } from './ZeppelinImage';
+import { getColorInfo } from './Colors';
+
+import { getHandAsSvgUrl } from './HandImage';
 
 // import './RealHeight';
 
 @customElement('test-app')
 export class TestApp extends LitElement {
   static get styles(): CSSResultArray {
-    const iconUrl = `data:image/svg+xml,${unsafeCSS(
-      getZeppelinAsSvgUrl(
-        darken('#FF0000', 0.2),
-        '#FF0000',
-        lighten('#FF0000', 0.1)
+    const iconUrl1 = `data:image/svg+xml,${unsafeCSS(
+      getHandAsSvgUrl(
+        getColorInfo('purple').mainColorCode,
+        getColorInfo('orange').mainColorCode,
+        1
+      )
+    )}`;
+    const iconUrl2 = `data:image/svg+xml,${unsafeCSS(
+      getHandAsSvgUrl(
+        getColorInfo('purple').mainColorCode,
+        getColorInfo('orange').mainColorCode,
+        2
+      )
+    )}`;
+    const iconUrl3 = `data:image/svg+xml,${unsafeCSS(
+      getHandAsSvgUrl(
+        getColorInfo('purple').mainColorCode,
+        getColorInfo('orange').mainColorCode,
+        3
+      )
+    )}`;
+    const iconUrl4 = `data:image/svg+xml,${unsafeCSS(
+      getHandAsSvgUrl(
+        getColorInfo('purple').mainColorCode,
+        getColorInfo('orange').mainColorCode,
+        4
+      )
+    )}`;
+    const iconUrl5 = `data:image/svg+xml,${unsafeCSS(
+      getHandAsSvgUrl(
+        getColorInfo('purple').mainColorCode,
+        getColorInfo('orange').mainColorCode,
+        5
+      )
+    )}`;
+    const iconUrl6 = `data:image/svg+xml,${unsafeCSS(
+      getHandAsSvgUrl(
+        getColorInfo('purple').mainColorCode,
+        getColorInfo('orange').mainColorCode,
+        6
+      )
+    )}`;
+    const iconUrl7 = `data:image/svg+xml,${unsafeCSS(
+      getHandAsSvgUrl(
+        getColorInfo('purple').mainColorCode,
+        getColorInfo('orange').mainColorCode,
+        7
+      )
+    )}`;
+    const iconUrl8 = `data:image/svg+xml,${unsafeCSS(
+      getHandAsSvgUrl(
+        getColorInfo('purple').mainColorCode,
+        getColorInfo('orange').mainColorCode,
+        8
+      )
+    )}`;
+    const iconUrl9 = `data:image/svg+xml,${unsafeCSS(
+      getHandAsSvgUrl(
+        getColorInfo('purple').mainColorCode,
+        getColorInfo('orange').mainColorCode,
+        9
+      )
+    )}`;
+    const iconUrl10 = `data:image/svg+xml,${unsafeCSS(
+      getHandAsSvgUrl(
+        getColorInfo('purple').mainColorCode,
+        getColorInfo('orange').mainColorCode,
+        10
       )
     )}`;
 
     const a = css`
       #div1 {
-        height: 150px;
-        width: 400px;
+        background-size: 200px 200px;
+        height: 200px;
+        width: 200px;
         color: black;
         border: 2px black solid;
-        background-color: #e6194b;
+        background-image: url('${unsafeCSS(iconUrl1)}');
       }
       #div2 {
-        height: 150px;
-        width: 400px;
+        background-size: 200px 200px;
+        height: 200px;
+        width: 200px;
         color: black;
         border: 2px black solid;
-        background-color: ${unsafeCSS(darken('#E6194B', 0.1))};
+        background-image: url('${unsafeCSS(iconUrl2)}');
       }
       #div3 {
-        height: 150px;
-        width: 400px;
+        background-size: 200px 200px;
+        height: 200px;
+        width: 200px;
         color: black;
         border: 2px black solid;
-        background-image: url('${unsafeCSS(iconUrl)}');
+        background-image: url('${unsafeCSS(iconUrl3)}');
+      }
+      #div4 {
+        background-size: 200px 200px;
+        height: 200px;
+        width: 200px;
+        color: black;
+        border: 2px black solid;
+        background-image: url('${unsafeCSS(iconUrl4)}');
+      }
+      #div5 {
+        background-size: 200px 200px;
+        height: 200px;
+        width: 200px;
+        color: black;
+        border: 2px black solid;
+        background-image: url('${unsafeCSS(iconUrl5)}');
+      }
+      #div6 {
+        background-size: 200px 200px;
+        height: 200px;
+        width: 200px;
+        color: black;
+        border: 2px black solid;
+        background-image: url('${unsafeCSS(iconUrl6)}');
+      }
+      #div7 {
+        background-size: 200px 200px;
+        height: 200px;
+        width: 200px;
+        color: black;
+        border: 2px black solid;
+        background-image: url('${unsafeCSS(iconUrl7)}');
+      }
+      #div8 {
+        background-size: 200px 200px;
+        height: 200px;
+        width: 200px;
+        color: black;
+        border: 2px black solid;
+        background-image: url('${unsafeCSS(iconUrl8)}');
+      }
+      #div9 {
+        background-size: 200px 200px;
+        height: 200px;
+        width: 200px;
+        color: black;
+        border: 2px black solid;
+        background-image: url('${unsafeCSS(iconUrl9)}');
+      }
+      #div10 {
+        background-size: 200px 200px;
+        height: 200px;
+        width: 200px;
+        color: black;
+        border: 2px black solid;
+        background-image: url('${unsafeCSS(iconUrl10)}');
       }
     `;
 
@@ -47,8 +171,19 @@ export class TestApp extends LitElement {
   }
 
   protected render(): HTMLTemplateResult {
-    return html` <div id="div1">test1</div>
-      <div id="div2">test2</div>
-      <div id="div3">Test3</div>`;
+    return html` <hand-with-dots
+        numberDots="5"
+        style="width: 200px; height: 200px; --hand-color: #ff00ff; --dot-color:green;"
+      ></hand-with-dots>
+      <div id="div1"></div>
+      <div id="div2"></div>
+      <div id="div3"></div>
+      <div id="div4"></div>
+      <div id="div5"></div>
+      <div id="div6"></div>
+      <div id="div7"></div>
+      <div id="div8"></div>
+      <div id="div9"></div>
+      <div id="div10"></div>`;
   }
 }
