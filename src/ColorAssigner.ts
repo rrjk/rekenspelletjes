600c9413b820ad0b79a584ab37585bdad590c3f0
Ronald Koster <rk-github@fam-koster.nl>
2024-03-18 19:24:49 +0000

    Incorrect attempt color assigner algo.

--- src/ColorAssigner.ts
+++ src/ColorAssigner.ts
@@ -4,16 +4,22 @@ import { shuffleArray } from './Randomizer';
 type Coordinate = { x: number; y: number };
 
 type ColorLocationArray = { color: Color; coordinates: Coordinate[] }[];
+type NumberColorLocationArray = {
+  nmbr: number;
+  color: Color;
+  coordinates: Coordinate[];
+}[];
 
 export class ColorAssigner {
   colorMatrix: Color[][] = [[]];
   digitMatrix: number[][] = [[]];
-  numberQuestions = 8;
+  numberQuestions = 10;
 
   calculateNumberAssignment() {
     // Loop through colorMatrix to determine how many of each color we have
 
     const colorLocations: ColorLocationArray = [];
+    const nmbrColorLocations: NumberColorLocationArray = [];
 
     for (let x = 0; x < this.colorMatrix[0].length; x++) {
       for (let y = 0; y < this.colorMatrix.length; y++) {
@@ -27,13 +33,12 @@ export class ColorAssigner {
       }
     }
 
-    console.log(colorLocations);
+    console.log(JSON.stringify(colorLocations));
 
     for (const colorLocation of colorLocations) {
-      console.log(colorLocation);
       shuffleArray(colorLocation.coordinates);
-      console.log(colorLocation);
     }
+    console.log(JSON.stringify(colorLocations));
 
     const numberColors = colorLocations.length;
     if (numberColors > this.numberQuestions)
@@ -48,14 +53,46 @@ export class ColorAssigner {
     const averageNumberBlocksPerFreeQuestion =
       (numberBlocks - numberColors) / numberFreeQuestions;
 
-    console.log(colorLocations);
+    let nextQuestionNumber = 1;
+    for (const colorLocation of colorLocations) {
+      console.log(`color: ${colorLocation.color}`);
+      const numberQuestionsForColor = Math.max(
+        1,
+        Math.floor(
+          (colorLocation.coordinates.length - 1) /
+            averageNumberBlocksPerFreeQuestion
+        )
+      );
+      console.log(`nmbrQuestionsForColor = ${numberQuestionsForColor}`);
+
+      const numberCoordinatesPerQuestion = Math.floor(
+        colorLocation.coordinates.length / numberQuestionsForColor
+      );
+      for (let i = 0; i < numberQuestionsForColor; i++) {
+        const coordinatesForQuestion = [];
+
+        for (let j = 0; j < numberCoordinatesPerQuestion; j++) {
+          const coordinate = colorLocation.coordinates.pop();
+          if (coordinate === undefined)
+            throw new Error(
+              'Internal SW error, more coordinates expected but none found'
+            );
+          coordinatesForQuestion.push(coordinate);
+        }
+
+        nmbrColorLocations.push({
+          nmbr: nextQuestionNumber,
+          color: colorLocation.color,
+          coordinates: coordinatesForQuestion,
+        });
+        nextQuestionNumber += 1;
+      }
+    }
+
+    console.log(JSON.stringify(colorLocations));
+    console.log(JSON.stringify(nmbrColorLocations));
     console.log(numberFreeQuestions);
     console.log(numberBlocks);
     console.log(averageNumberBlocksPerFreeQuestion);
-
-    const array = [{ x: 0 }, { x: 1 }, 2, { x: 3 }, 4, { x: 5 }, 6];
-    console.log(array);
-    shuffleArray(array);
-    console.log(array);
   }
 }
