e802705408f654a9b74cf751d1947c83a0fcb61b
Ronald Koster <ronald@fam-koster.nl>
2024-08-28 20:58:09 +0200

    Intermediate question creation widget

--- src/QuestionCreationWidget.ts
+++ src/QuestionCreationWidget.ts
@@ -4,10 +4,13 @@ import type { HTMLTemplateResult, CSSResultGroup } from 'lit';
 // eslint-disable-next-line import/extensions
 import { customElement, property, state } from 'lit/decorators.js';
 
+type StringFieldEnum = 'question' | 'correctAnswer';
+type StringArrayFieldEnum = 'alternativeAnswers';
+
 @customElement('question-creation-widget')
 export class QuestionCreationWidget extends LitElement {
-  @state()
-  question = '';
+  @property()
+  question = 'blaat';
 
   @state()
   correctAnswer = '';
@@ -18,17 +21,80 @@ export class QuestionCreationWidget extends LitElement {
   static get styles(): CSSResultGroup {
     return css`
       :host {
-        display: inline-block;
+        display: grid;
+        grid-template-columns: 10rem 40rem;
+        grid-template-rows: repeat(4, 20px);
         background-color: #cccccc;
-        border-radius: 10%;
-        border: 0px;
+        border: 1px;
       }
     `;
   }
 
+  handleInputChangeUpdate(
+    evt: Event,
+    fieldName: StringFieldEnum | StringArrayFieldEnum,
+    arrayIndex = 0
+  ) {
+    const elm = <HTMLInputElement>evt.target;
+    if (elm !== null && fieldName !== undefined) {
+      if (fieldName === 'question' || fieldName === 'correctAnswer')
+        this[fieldName] = elm.value;
+      else this[fieldName][arrayIndex] = elm.value;
+    }
+  }
+
+  updatePropertyFromInput(
+    elm: HTMLInputElement | null,
+    toUpdate: StringFieldEnum
+  ) {
+    if (elm !== null && toUpdate !== undefined) {
+      this[toUpdate] = elm.value;
+    }
+  }
+
+  updatePropertyElementFromInput(
+    elm: HTMLInputElement | null,
+    toUpdate: StringArrayFieldEnum,
+    index: number
+  ) {
+    if (elm != null && toUpdate !== undefined && index !== undefined) {
+      this[toUpdate][index] = elm.value;
+    }
+    console.log(this.alternativeAnswers);
+  }
+
   render(): HTMLTemplateResult {
     return html`
-      <input type="text" id="question" name="Vraag" maxlength="80" />
+      <div><b>Vraag</b></div>
+      <div>
+        <input
+          type="text"
+          label="vraag"
+          maxlength="80"
+          size="40"
+          .value=${this.question}
+          @change=${(evt: Event) =>
+            this.updatePropertyFromInput(
+              <HTMLInputElement>evt.target,
+              'question'
+            )}
+        />
+      </div>
+      <div><b>Juiste antwoord</b></div>
+      <div>
+        <input
+          type="text"
+          label="juiste antwoord"
+          maxlength="80"
+          size="40"
+          .value=${this.question}
+          @change=${(evt: Event) =>
+            this.updatePropertyFromInput(
+              <HTMLInputElement>evt.target,
+              'correctAnswer'
+            )}
+        />
+      </div>
     `;
   }
 }
