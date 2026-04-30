6311eb670d3b8c0da31d127f1bd9c04e07294d37
Ronald Koster <ronald@fam-koster.nl>
2026-04-30 10:20:38 +0200

    Fix path references in URLshortener2 and rollup config to repair t shortcut handling.
    - Update baseUrl path in URLshortener2.ts from ../ to ../../
    - Fix input path in t/rollup.config.ts from s/index.html to t/index.html

--- src/URLshortener2.ts
+++ src/URLshortener2.ts
@@ -8,7 +8,7 @@ import {
 const urlParams = new URLSearchParams(window.location.search);
 const key = urlParams.keys().next().value;
 
-const baseUrl = new URL(`../Rekenspelletjes/`, import.meta.url);
+const baseUrl = new URL(`../../Rekenspelletjes/`, import.meta.url);
 const defaultUrl = new URL('./index.html', baseUrl);
 
 const baseURLs: Partial<Record<string, URL>> = {
