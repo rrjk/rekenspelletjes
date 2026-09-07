a5bfbfcbe95ba5db9f769e43f10689cc5f36f764
Ronald Koster <ronald@fam-koster.nl>
2026-09-07 20:56:23 +0200

    Update TestApp to test EditableText widget

--- src/TestApp.ts
+++ src/TestApp.ts
@@ -1,87 +1,39 @@
 import { html, css, LitElement } from 'lit';
 import type { CSSResultArray, HTMLTemplateResult } from 'lit';
 
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 
-import './MathStepsIndexApp/MathStepSumsIcon';
+import './GoalCardIndexApp/EditableText';
+import { type ValueChangedEvent } from './GoalCardIndexApp/EditableText';
 
 @customElement('test-app')
 export class TestApp extends LitElement {
+  @state()
+  accessor value = 'Ronald';
+
   static get styles(): CSSResultArray {
     return [
       css`
-        :host {
-          width: 100%;
-          height: 100%;
-          display: block;
-          padding: 20px;
-        }
-
-        div.space {
-          display: block;
-          position: relative;
-          height: 20px;
-        }
-
-        math-step-sums-icon#largeWide {
-          width: 800px;
-          height: 36px;
-        }
-        math-step-sums-icon#mediumWide {
-          width: 500px;
-          height: 24px;
-        }
-        math-step-sums-icon#smallWide {
-          width: 300px;
-          height: 12px;
-        }
-
-        math-step-sums-icon#largeTall {
-          width: 636px;
-          height: 50px;
-        }
-        math-step-sums-icon#mediumTall {
-          width: 424px;
-          height: 40px;
-        }
-        math-step-sums-icon#smallTall {
-          width: 212px;
-          height: 30px;
+        editable-text {
+          --font-size: 70px;
+          --font-weight: bold;
         }
       `,
     ];
   }
 
+  valueChanged(e: ValueChangedEvent) {
+    this.value = e.value;
+  }
+
   protected renderTest(): HTMLTemplateResult {
-    return html` <math-step-sums-icon
-        id="smallWide"
-        math-step="PlusMinusTill10"
-      ></math-step-sums-icon>
-      <div class="space"></div>
-      <math-step-sums-icon
-        id="mediumWide"
-        math-step="PlusMinusTill10"
-      ></math-step-sums-icon>
-      <div class="space"></div>
-      <math-step-sums-icon
-        id="largeWide"
-        math-step="PlusMinusTill10"
-      ></math-step-sums-icon>
-      <div class="space"></div>
-      <math-step-sums-icon
-        id="smallTall"
-        math-step="PlusMinusTill10"
-      ></math-step-sums-icon>
-      <div class="space"></div>
-      <math-step-sums-icon
-        id="mediumTall"
-        math-step="PlusMinusTill10"
-      ></math-step-sums-icon>
-      <div class="space"></div>
-      <math-step-sums-icon
-        id="largeTall"
-        math-step="PlusMinusTill10"
-      ></math-step-sums-icon>`;
+    return html`
+      <editable-text
+        value=${this.value}
+        @value-changed=${(e: ValueChangedEvent) => this.valueChanged(e)}
+      ></editable-text>
+      <h2>Piet</h2>
+    `;
   }
 
   protected render(): HTMLTemplateResult {
